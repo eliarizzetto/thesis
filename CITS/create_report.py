033002e63ca6a2e38e58aaf14ec17503afd6dd5b
eliarizzetto <elia.rizzetto@gmail.com>
2022-10-25 04:27:53 +0200

    upload for session 009: added fundamental features
    This version allows to save the errors in semantically richer dictionaries (one for each function), that have a fixed and consistent structure (see issue raised in meeting 008).
    I hade updated:
    - the json schemas
    - the function for creating the report dictionary
    - the structure of the main body, specifically the way the validating functions are called and other modifications (major changes here!!!)

--- CITS/create_report.py
+++ CITS/create_report.py
@@ -2,25 +2,21 @@
 # about an error (i.e. create a single error report).
 
 
-def create_error_dict(validation_level: str, error_type: str, message: str, row: int, field=None, idx_in_field=None):
+def create_error_dict(validation_level: str, error_type: str, message: str, located_in: str, row: list, field=None, item=None):
     """
     Creates a dictionary with the information about an error
     :param validation_level: str, one of the following values: 'csv_wellformedness', 'external_syntax', 'semantic'
     :param error_type: str, one of the following values: 'error', 'warning'
     :param message: str, the message explaining the error to display to the user
-    :param row: int, the index of the row in which the error is located
-    :param field: (Optional) str, one of the following values: "id", "title", "author", "pub_date", "venue", "volume", "issue",
+    :param located_in: str, the type of the area where the error is located, i.e. ONE among the following: "row", "field", "item".
+    :param row: list of one or more integers, i.e. the indexes of the rows in which the error is located
+    :param field: either None or a list of strings among the following values: "id", "title", "author", "pub_date", "venue", "volume", "issue",
     "page", "type", "publisher", "editor", "citing_id", "citing_publication_date", "cited_id", "cited_publication_date".
     Must be specified whenever the error is inside a specific field.
-    :param idx_in_field: (Optional) int, the index of a list element
+    :param item: either None or a list of integers, i.e. the indexes of a field's items where the errors are located.
     :return: dict with the error's details
     """
-    position = {'row': row}
-
-    if field is not None:
-        position['field'] = field
-    if idx_in_field is not None:
-        position['idx_in_field'] = idx_in_field
+    position = {'located_in': located_in, 'row': row, 'field': field, 'item': item}
 
     result = {
         'validation_level': validation_level,
--- CITS/validate_cits.py
+++ CITS/validate_cits.py
@@ -4,6 +4,7 @@ from helper_functions import content
 from validation_functions import wellformedness_id_field, wellformedness_single_id, wellformedness_date
 from create_report import create_error_dict
 from check_output.check_validation_output import check_validation_output
+import re
 from pprint import pprint
 
 # sample_row = {
@@ -25,17 +26,28 @@ for field, value in sample_invalid_row.items():
     if content(value):
 
         if field == 'citing_id' or field == 'cited_id':
+            items = re.split('\s', value)
+
             if not wellformedness_id_field(value):
-                message = "The value in this field is not expressed in compliance with the syntax of OpenCitation " \
+                message = "The value in this field is not expressed in compliance with the syntax of OpenCitations " \
                           "CITS-CSV. The content of 'citing_id' and 'cited_id' must be either a single ID or a sequence" \
                           " of IDs, each separated by one single space character (Unicode Character “SPACE”, U+0020). " \
                           "Each ID must not have spaces within itself, and must be of the following form: " \
                           "ID abbreviation + “:” + ID value. "
 
-                error_final_report.append(create_error_dict(validation_level='csv_wellformedness', error_type='error', message=message,
-                                          row=0, field=field))
+                wrong_items_idxs = []  # find and append to list which item locations raise the error (extra spaces)
+                for idx, item in enumerate(items):
+                    if item == '':
+                        wrong_items_idxs.append(idx)
 
-            for id_idx, id in enumerate(value.split()):
+                error_final_report.append(
+                    create_error_dict(validation_level='csv_wellformedness', error_type='error', message=message,
+                                      located_in='row',
+                                      row=[0], field=[field], item=wrong_items_idxs))
+
+
+            # badly_formed_items_idxs = []  # list with items that share the same error (failed well-formedness)
+            for id_idx, id in enumerate(items):
 
                 if not wellformedness_single_id(id):
                     message = "The value in this field is not expressed in compliance with the syntax of OpenCitations " \
@@ -44,8 +56,16 @@ for field, value in sample_invalid_row.items():
                               "doi:10.48550/arXiv.2206.03971. The accepted prefixes (ID abbreviations) are the " \
                               "following: 'doi', 'issn', 'isbn', 'pmid', 'pmcid', 'url', 'wikidata', 'wikipedia'. "
 
-                    error_final_report.append(create_error_dict(validation_level='csv_wellformedness', error_type='error',
-                                              message=message, row=0, field=field, idx_in_field=id_idx))
+                    # badly_formed_items_idxs.append(id_idx)
+                    error_final_report.append(
+                        create_error_dict(validation_level='csv_wellformedness', error_type='error',
+                                          message=message, located_in='item', row=[0], field=[field], item=[id_idx]))
+
+            # if badly_formed_items_idxs != []:
+            #     error_final_report.append(
+            #         create_error_dict(validation_level='csv_wellformedness', error_type='error',
+            #                           message=message, located_in='item', row=[0], field=[field], item=badly_formed_items_idxs))
+
 
                     # -------------ADD CHECK ON LEVEL 2 (EXTERNAL SYNTAX) AND 3 (SEMANTICS) FOR THE SINGLE IDs
 
@@ -58,7 +78,7 @@ for field, value in sample_invalid_row.items():
                           "the day must also be expressed. Examples: '2000'; '2000-04'; '2000-04-27'."
 
                 error_final_report.append(create_error_dict(validation_level='csv_wellformedness', error_type='error',
-                                          message=message, row=0, field=field))
-
+                                                            message=message, located_in='field', row=[0],
+                                                            field=[field]))
 
 pprint(error_final_report)
