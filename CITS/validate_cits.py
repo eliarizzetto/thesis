1f8c325eb96eabb5261e7004df458eecab107ee8
eliarizzetto <elia.rizzetto@gmail.com>
2022-11-08 02:15:12 +0100

    solved issue on duplication
    This version allows to call in the main structure:
    - a function to get the bibliographic entity (i.e. the set of co-occurring IDs) corresponding to each id -> group_ids() in .helper_functions.py
    - a function to find self-citations and duplicate citations ('get_duplicates_cits()) inside get_duplicates.py.

--- CITS/validate_cits.py
+++ CITS/validate_cits.py
@@ -1,30 +1,32 @@
 # This file contains the base structure wherein to call the validation functions for CITS-CSV.
 
-from helper_functions import content
+from helper_functions import content, group_ids
 from validation_functions import wellformedness_id_field, wellformedness_single_id, wellformedness_date
 from create_report import create_error_dict
-from check_output.check_validation_output import check_validation_output
+# from check_output.check_validation_output import check_validation_output
 import re
 from pprint import pprint
 import yaml
 from csv import DictReader
-from helper_functions import group_ids
-
+from get_duplicates import get_duplicates_cits
 
 csv_doc = 'C:/Users/media/Desktop/thesis23/thesis_resources/validation_process/validation/test_files/sample_cits.csv'
 
 with open(csv_doc, 'r', encoding='utf-8') as f:
-    data_dict = DictReader(f)
+    data_dict = list(DictReader(f))
 
     error_final_report = []
 
     messages = yaml.full_load(open('messages.yaml', 'r', encoding='utf-8'))
 
-    for row_idx, row in enumerate(data_dict):
+    id_fields_instances = []
+    # set_unique_items = set() # ----------------!!!
 
+    for row_idx, row in enumerate(data_dict):
         for field, value in row.items():
             if content(value):
                 if field == 'citing_id' or field == 'cited_id':
+                    ids_set = set()  # set where to put valid IDs only
                     items = re.split('\s', value)
 
                     for item_idx, item in enumerate(items):
@@ -44,10 +46,21 @@ with open(csv_doc, 'r', encoding='utf-8') as f:
                                                   message=message, located_in='item', table=table))
 
                         else:
-                            # -------------ADD CHECK ON LEVEL 2 (EXTERNAL SYNTAX) AND 3 (SEMANTICS) FOR THE SINGLE IDs
-                            pass
+                            # TODO: ADD CHECK ON LEVEL 2 (EXTERNAL SYNTAX) AND 3 (SEMANTICS) FOR THE SINGLE IDs
 
+                            if item not in ids_set:
+                                ids_set.add(item)
+                            else:  # in-field duplication of the same ID
+                                table = {row: {field: [i for i, v in enumerate(item) if v == item]}}
+                                message = messages['m6']
 
+                                error_final_report.append(
+                                    create_error_dict(validation_level='csv_wellformedness', error_type='warning',
+                                                      message=message, located_in='item', table=table, valid=True)
+                                )
+
+                    if len(ids_set) >= 1:
+                        id_fields_instances.append(ids_set)
 
                 if field == 'citing_publication_date' or field == 'cited_publication_date':
                     if not wellformedness_date(value):
@@ -57,5 +70,13 @@ with open(csv_doc, 'r', encoding='utf-8') as f:
                             create_error_dict(validation_level='csv_wellformedness', error_type='error',
                                               message=message, located_in='item', table=table))
 
-pprint(error_final_report, sort_dicts=False)
-# print(error_final_report)
+    # GET BIBLIOGRAPHIC ENTITIES
+    entities = group_ids(id_fields_instances)
+
+    # GET SELF-CITATIONS AND DUPLICATE CITATIONS
+    duplicate_report = get_duplicates_cits(entities=entities, data_dict=data_dict)
+
+    if duplicate_report:
+        error_final_report.append(duplicate_report)
+
+pprint(error_final_report)
